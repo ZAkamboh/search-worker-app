c817aa9ae97d41ebae1d85ce1ec7c35ed879666f
zubair aslam <zubairkamboh602@gmail.com>
2018-06-13 02:14:48 -0700

    change in models

--- models/user.js
+++ models/user.js
@@ -12,28 +12,27 @@ type:String
 email:{
 
     type:String
-    
+
     },
 
     password:{
 
     type:String
-    
+
     },
     select:{
 
         type:String
-        
+
         },
         file:{
 
             type:String
-            
+
             }
-            
-        
-    
+
+
+
 
 })
 module.exports=mongoose.model('helpersignup', helperschema);
-
